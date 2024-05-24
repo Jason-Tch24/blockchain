f968ade6057bda52292272fdd315f8d367f8eea1
Adeline Kameni <kameniadeline823@gmail.com>
2024-05-24 15:53:11 +0200

    initialisation du projet

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import hashlib
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class GeekCoinBlock:
+    def __init__(self, previous_block_hash, transaction_list):
+        self.previous_block_hash = previous_block_hash
+        self.transaction_list = transaction_list
+        self.block_data = f"{' - '.join(transaction_list)} - {previous_block_hash}"
+        self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
 
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.generate_genesis_block()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def generate_genesis_block(self):
+        self.chain.append(GeekCoinBlock("0", ['Genesis Block']))
 
+    def create_block_from_transaction(self, transaction_list):
+        previous_block_hash = self.last_block.block_hash
+        self.chain.append(GeekCoinBlock(previous_block_hash, transaction_list))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def display_chain(self):
+        for i in range(len(self.chain)):
+            print(f"Block {i + 1} Data: {self.chain[i].block_data}")
+            print(f"Block {i + 1} Hash: {self.chain[i].block_hash}\n")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
+# Exemple d'utilisation
+t1 = "George envoie 3.1 GC à Joe"
+t2 = "Joe envoie 2.5 GC à Adam"
+t3 = "Adam envoie 1.2 GC à Bob"
+t4 = "Bob envoie 0.5 GC à Charlie"
+t5 = "Charlie envoie 0.2 GC à David"
+t6 = "David envoie 0.1 GC à Eric"
+
+myblockchain = Blockchain()
+myblockchain.create_block_from_transaction([t1, t2])
+myblockchain.create_block_from_transaction([t3, t4])
+myblockchain.create_block_from_transaction([t5, t6])
+
+myblockchain.display_chain()
